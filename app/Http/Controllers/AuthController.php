e0a28c99bc3ac9ff45891911d92480d14a287417
robertkumar <rob_kumar@outlook.com>
2025-12-21 13:51:23 -0500

    added ApiResponses trait

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\ApiResponses;
+
 class AuthController extends Controller
 {
-    //
+    use ApiResponses;
+
+    public function login() {
+        return response()->json([
+            'message' => 'Hello, Login'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'success'
-    ], 200);
-});
+Route::get('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
