24ca3cd4bc792152f70521bf4c8f06c9bbe889a7
robertkumar <rob_kumar@outlook.com>
2026-01-05 09:15:15 -0500

    Updated the API to include the new users apiResource

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // localhost:8000/api/v1
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
