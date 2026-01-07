be295ec623acb94f649443b2e1762fd720d23732
robertkumar <rob_kumar@outlook.com>
2026-01-07 13:30:21 -0500

    add filter method to iterate over filters in query string

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -14,6 +14,16 @@ abstract class QueryFilter
         $this->request = $request;
     }
 
+    protected function filter($arr ) {
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
     public function apply(Builder $builder): Builder
     {
         $this->builder = $builder;
