6d935bd9af23345dd3efe31e2768156a11909a72
robertkumar <rob_kumar@outlook.com>
2026-01-07 13:22:40 -0500

    add query filtering to the API calls for status on the index route

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -13,9 +13,9 @@ class Ticket extends Model
     /** @use HasFactory<\Database\Factories\TicketFactory> */
     use HasFactory;
 
-    public function user(): BelongsTo
+    public function author(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function scopeFilter(Builder $builder, QueryFilter $filters) {
