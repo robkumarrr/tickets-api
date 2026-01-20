5dd6a26cd442c7dd607506a588f8474ae4f1c636
robertkumar <rob_kumar@outlook.com>
2026-01-19 21:59:50 -0500

    WIP: filter authors

---
+++ app/Http/Filters/V1/AuthorFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Filters\V1;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class AuthorFilter extends QueryFilter
+{
+    public function include($value): Builder
+    {
+        return $this->builder->with($value);
+    }
+
+    public function createdAt($value): Builder {
+        $dates = explode(',', $value);
+
+        if (count($dates) > 1) {
+            return $this->builder->whereBetween('created_at', $dates);
+        }
+
+        return $this->builder->whereDate('created_at', $value);
+    }
+
+    public function updatedAt($value): Builder {
+        $dates = explode(',', $value);
+
+        if (count($dates) > 1) {
+            return $this->builder->whereBetween('updated_at', $dates);
+        }
+
+        return $this->builder->whereDate('updated_at', $value);
+    }
+
+    public function id($value): Builder {
+        return $this->builder->whereIn('id', explode(',', $value));
+    }
+
+    public function email($value): Builder {
+        $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('email', 'like', $likeStr);
+    }
+
+    public function name($value): Builder {
+        $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('name', 'like', $likeStr);
+    }
+}
