375ee3cb952921b2eb8f5adbf5a190b87d21997b
robertkumar <rob_kumar@outlook.com>
2026-01-23 22:34:29 -0500

    WIP authors ticket patch

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -33,6 +33,30 @@ class AuthorTicketsController extends ApiController
         return new TicketResource(Ticket::create($model));
     }
 
+    public function update(ReplaceAuthorTicketRequest $request, $author_id, $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($ticket->user_id == $author_id){
+                $model = [
+                    'title' => $request->input('data.attributes.title'),
+                    'description' => $request->input('data.attributes.description'),
+                    'status' => $request->input('data.attributes.status'),
+                    'user_id' => $author_id
+                ];
+
+                $ticket->update($model);
+
+                return new TicketResource($ticket);
+            }
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found', Response::HTTP_NOT_FOUND);
+        }
+        return $this->error('Ticket cannot be found', Response::HTTP_NOT_FOUND);
+    }
+
     public function replace(ReplaceAuthorTicketRequest $request, $author_id, $ticket_id)
     {
         try {
---
+++ app/Http/Requests/Api/V1/UpdateAuthorTicketRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAuthorTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'data.attributes.title' => ['sometimes', 'string'],
+            'data.attributes.description' => ['sometimes', 'string'],
+            'data.attributes.status' => ['sometimes', 'string', 'in:A,C,H,X'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'data.attributes.status' => 'The status is not one of A, C, H or X.',
+        ];
+    }
+}
