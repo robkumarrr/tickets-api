c96e570f453c09e967f84736a7f7d88ceb606d7b
robertkumar <rob_kumar@outlook.com>
2026-01-24 09:54:05 -0500

    add store class for author ticket controller

---
+++ app/Http/Requests/Api/V1/AuthorTickets/StoreAuthorTicketRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\AuthorTickets;
+
+use App\Http\Requests\Api\V1\Tickets\BaseTicketRequest;
+
+class StoreAuthorTicketRequest extends BaseTicketRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'data.attributes.title' => ['required', 'string'],
+            'data.attributes.description' => ['required', 'string'],
+            'data.attributes.status' => ['required', 'string', 'in:A,C,H,X'],
+        ];
+
+        if ($this->routeIs('tickets.store'))
+        {
+            $rules['data.relationships.author.data.id'] = ['required', 'integer'];
+        }
+
+        return $rules;
+    }
+}
